14e91a93c1b046bf0b2b3795695a6463fad54798
Daniil Zhelyazkov <daniilzhelyazkov@gmail.com>
2025-01-12 22:14:26 +0000

    Running tests

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,6 +4,7 @@ import portraitImage from "../images/portraitImage2-cr2.png";
 import portraitImageBackground from "../images/blob.svg";
 import { Helmet } from "react-helmet";
 export default function AboutMe() {
+  //Testing the automatic build
   return (
     <div className="row" name="scroll-here-intro">
       <div className="col-12 col-md-6">
@@ -18,19 +19,21 @@ export default function AboutMe() {
             alt=""
           />
           <div className="portraitBorderBack w-50 h-50 position-absolute"></div>
-            <img
-              className="w-50 portraitImage position-absolute"
-              id="portrait"
-              src={portraitImage}
-              alt="portrait of me"
-            />
+          <img
+            className="w-50 portraitImage position-absolute"
+            id="portrait"
+            src={portraitImage}
+            alt="portrait of me"
+          />
         </figure>
       </div>
       <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
         <article id="portraitText">
           <hgroup>
             <h1 className="nameText1">daniilZhelyazkov</h1>
-            <h5 className="text-white nameText2">myPortfolio<span className="blink">_</span></h5>
+            <h5 className="text-white nameText2">
+              myPortfolio<span className="blink">_</span>
+            </h5>
           </hgroup>
         </article>
       </div>
